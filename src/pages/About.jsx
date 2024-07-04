82b2295db5f651939e5c77199b93a13932707862
Yash1719 <yashagarwal1719@gmail.com>
2024-07-04 22:40:34 +0530

    frontend update

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>About </h1>
+    <p className='mb-4 text-slate-700'>BookMyStay is a premier real estate agency dedicated to assisting clients in buying, selling, and renting properties in prime locations. Our seasoned team of agents is committed to delivering outstanding service and ensuring a seamless real estate experience.</p>
+    <p className='mb-4 text-slate-700'>
+    Our goal is to support our clients in reaching their real estate aspirations through expert guidance, tailored service, and an in-depth knowledge of the local market. Whether you're in the market to purchase, sell, or lease a property, we're here to support you at every stage of the process.
+    </p>
+    <p className='mb-4 text-slate-700'>With extensive experience and expertise in the real estate sector, our team is dedicated to providing top-notch service to our clients. We strive to make the journey of buying or selling a property both exciting and rewarding, and we are devoted to turning that vision into reality for all of our clients.</p>
+  </div>
+  )
+}
